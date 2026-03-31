453756db869182244265d55f590873bdfbb4ea70
restuchan <restu.chan188@gmail.com>
2026-03-31 22:27:01 +0700

    code untuk button Hitung MK

--- PraktikumADO/PraktikumADO/Form1.cs
+++ PraktikumADO/PraktikumADO/Form1.cs
@@ -23,6 +23,27 @@ namespace PraktikumADO
             );
         }
 
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         private void btnHitungMhs_Click(object sender, EventArgs e)
         {
             try
